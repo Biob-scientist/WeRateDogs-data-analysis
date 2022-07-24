eebc59e4d61a3522bfb42576ab88f2b9c07b3c0e
dukkya <aduewaoluwasola@gmail.com>
2022-07-24 09:56:08 +0100

    first commit

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,4 @@
+consumer_key='hf0ssLvi9XPumKEvMMdCnvSVw'
+consumer_secret='3V67OGrqAy5SKdZok39J2tcB4kfDzl0xzGTceaJjI6Km1sRhrR'
+access_token='1154455336465653760-rjBtbo7bFztL4Kubg6FAiq1lKTWVqy'
+access_secret='6mrV0l1mIEXYK6DW8QPGdEUrgZVa95EC8otSxQKfiZ92J'
